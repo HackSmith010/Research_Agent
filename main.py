e5aacdd9e68e40c39ae7c0321a41b7454ffaa751
HackSmith010 <anshujaiswal119@gmail.com>
2025-06-23 20:55:01 +0530

    Project Started

---
+++ main.py
@@ -0,0 +1,17 @@
+from mcp import ClientSession,StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import  load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    gemini_api_key=os.getenv("GEMINI_API_KEY"),
+    
+)
